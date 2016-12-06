c123048dd04c1e44eaee23e5cf89a1cec497b748
Raghavendra D Prabhu <me@rdprabhu.com>
2016-12-06 23:03:51 +0000

    Updated run.sh for makepkg

--- run.sh
+++ run.sh
@@ -7,11 +7,13 @@ export CXXFLAGS='-march=x86-64 -O2 -pipe'
 # This is just for testing
 srcname=$(grep '_srcname=' PKGBUILD | cut -d= -f2)
 
+/usr/sbin/makepkg --skippgpcheck  --nosign -o
+
 pushd src/$srcname
     for pp in /patches/*;do 
         patch -p1 -i $pp 
     done
 popd 
-/usr/sbin/makepkg --skippgpcheck  --nosign 
+/usr/sbin/makepkg --skippgpcheck  --nosign  -e
 
 cp -v *.pkg.tar.* $dest/
