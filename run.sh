f4f45becb7429ceddcdb6a00f2dd41396bf4b217
Raghavendra D Prabhu <me@rdprabhu.com>
2016-12-06 21:58:16 +0000

    Update pkgbuild

--- run.sh
+++ run.sh
@@ -6,6 +6,8 @@ export MAKEFLAGS=-j2
 export CFLAGS='-march=x86-64 -O2 -pipe'
 export CXXFLAGS='-march=x86-64 -O2 -pipe'
 # This is just for testing
+wget -O - https://git.archlinux.org/svntogit/packages.git/snapshot/packages-packages/htop.tar.gz | tar zxf -
+cd  packages-packages/htop/repos/extra-x86_64/
 /usr/sbin/makepkg --skippgpcheck  --nosign 
 
-cp *.pkg.tar.gz $dest/
+cp -v *.pkg.tar *.pkg.tar.gz $dest/
