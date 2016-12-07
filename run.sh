05277aa1277f4020b3b3e62c393d4e12be4781ef
Raghavendra D Prabhu <me@rdprabhu.com>
2016-12-07 17:05:12 +0000

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,16 +4,19 @@ rm *.pkg.tar.*
 export MAKEFLAGS=-j2
 export CFLAGS='-march=x86-64 -O2 -pipe'
 export CXXFLAGS='-march=x86-64 -O2 -pipe'
-# This is just for testing
+
+gpg --recv-keys 647F28654894E3BD457199BE38DBBDC86092693E
+gpg --recv-keys 79BE3E4300411886
+
 srcname=$(grep '_srcname=' PKGBUILD | cut -d= -f2)
 
-/usr/sbin/makepkg --skippgpcheck  --nosign -o
+/usr/sbin/makepkg  --nosign -o
 
 pushd src/$srcname
     for pp in /patches/*;do 
         patch -p1 -i $pp 
     done
 popd 
-/usr/sbin/makepkg --skippgpcheck  --nosign  -e
+/usr/sbin/makepkg --nosign  -e
 
 cp -v *.pkg.tar.* $dest/
